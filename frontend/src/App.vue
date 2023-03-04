39bed9bd5914822e58bd00223a5fcf8abc9362ca
roland372 <damfat94@gmail.com>
2023-03-04 20:51:52 +0100

    added pinia store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,2 +1,15 @@
-<template>hello</template>
-<script setup lang="ts"></script>
+<template>
+  <div>hello</div>
+  <div>store test {{ count }}</div>
+</template>
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useMediaTrackerStore } from "@/stores/useMediaTrackerStore";
+
+const mediaTrackerStore = useMediaTrackerStore();
+const { count } = storeToRefs(mediaTrackerStore);
+
+setInterval(() => {
+  mediaTrackerStore.increment();
+}, 1000);
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
 
-createApp(App).use(Quasar, quasarUserOptions).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(Quasar, quasarUserOptions).mount("#app");
