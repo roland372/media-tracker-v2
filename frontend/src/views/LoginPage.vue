cc4a2c20b6a303992a39ad45c9a9ec94aa8a4572
Damian Fatyga <damfat94@gmail.com>
2023-05-14 02:19:58 +0200

    styled login page

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,16 +1,47 @@
 <template>
   <HeaderComponent title="Login">
-    <div>
-      <button :onclick="login">login</button>
+    <div class="page-container d-flex align-center justify-center">
+      <button @click="login" class="google-button">
+        <span class="google-icon">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="Google Logo"
+          />
+        </span>
+        Sign in with Google
+      </button>
     </div>
   </HeaderComponent>
 </template>
 <script setup lang="ts">
-import { onMounted } from "vue";
 import HeaderComponent from "@/components/media/HeaderComponent.vue";
 import { login } from "@/utils/auth";
-
-onMounted(async () => {
-  console.log("LOGIN MOUNTED");
-});
 </script>
+<style scoped>
+.google-button {
+  padding: 8px 16px;
+  border: 1px solid #ccc;
+  background-color: white;
+  color: #333;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.google-icon {
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  margin-right: 8px;
+  vertical-align: middle;
+}
+
+.google-icon img {
+  width: 100%;
+  height: 100%;
+}
+
+.page-container {
+  min-height: 75vh;
+}
+</style>
