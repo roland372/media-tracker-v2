d96b01d4bc450b0e60aec371afbdd3f8066a74e8
Damian Fatyga <damfat94@gmail.com>
2023-10-19 13:51:26 +0200

    backup before modifying authentication

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  req.user ? next() : res.sendStatus(401);
+  req.user ? next() : res.sendStatus(401); 
 };
