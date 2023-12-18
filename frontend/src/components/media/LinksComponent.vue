6a7c892d15661546017a60c4708e966db2ff3ba8
Damian Fatyga <damfat94@gmail.com>
2023-12-18 22:20:05 +0100

    updated links component

--- frontend/src/components/media/LinksComponent.vue
+++ frontend/src/components/media/LinksComponent.vue
@@ -1,81 +1,30 @@
 <template>
   <CardComponent title="Links">
-    <div class="d-flex flex-wrap justify-space-between align-start">
-      <section class="text-start pa-2">
-        <h3 class="text-color">Anime</h3>
-        <div v-for="link in usefulLinks.anime" :key="link.url">
+    <div
+      class="d-flex flex-wrap justify-space-between align-start flex-fill align-stretch"
+    >
+      <section
+        v-for="(category, catIdx) in Object.entries(usefulLinks)"
+        :key="catIdx"
+        class="text-start pa-2"
+      >
+        <h3 class="text-color text-capitalize text-center">
+          {{ category[0] }}
+        </h3>
+        <div v-for="(link, linkIdx) in category[1]" :key="linkIdx" class="py-2">
           <a
             class="link-color"
             :href="link.url"
             rel="noreferrer"
             target="_blank"
           >
-            {{ link.text }}
-          </a>
-        </div>
-      </section>
-      <section class="text-start pa-2">
-        <h3 class="text-color">Books</h3>
-        <div v-for="link in usefulLinks.books" :key="link.url">
-          <a
-            class="link-color"
-            :href="link.url"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </div>
-      </section>
-      <section class="text-start pa-2">
-        <h3 class="text-color">Games</h3>
-        <div v-for="link in usefulLinks.games" :key="link.url">
-          <a
-            class="link-color"
-            :href="link.url"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </div>
-      </section>
-      <section class="text-start pa-2">
-        <h3 class="text-color">Manga</h3>
-        <div v-for="link in usefulLinks.manga" :key="link.url">
-          <a
-            class="link-color"
-            :href="link.url"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </div>
-      </section>
-      <section class="text-start pa-2">
-        <h3 class="text-color">Movies</h3>
-        <div v-for="link in usefulLinks.movies" :key="link.url">
-          <a
-            class="link-color"
-            :href="link.url"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </div>
-      </section>
-      <section class="text-start pa-2">
-        <h3 class="text-color">Other</h3>
-        <div v-for="link in usefulLinks.other" :key="link.url">
-          <a
-            class="link-color"
-            :href="link.url"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {{ link.text }}
+            <div class="d-flex flex-column justify-center align-center">
+              <v-img
+                :src="`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${link.url}&size=256`"
+                width="50"
+              />
+              {{ link.text }}
+            </div>
           </a>
         </div>
       </section>
