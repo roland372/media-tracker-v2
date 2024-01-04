c992cb69b2764794de4d9f2d930fb8afaf3936bb
Damian Fatyga <damfat94@gmail.com>
2024-01-04 09:41:41 +0100

    disabled genshin and honkai routes

---
+++ frontend/src/views/GenshinImpactPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <CardComponent title="Genshin Impact"></CardComponent>
+  <CardComponent title="Domains"></CardComponent>
+  <CardComponent title="Characters"></CardComponent>
+  <CardComponent title="Builds"></CardComponent>
+  <CardComponent title="Banner Stats"></CardComponent>
+  <CardComponent title="Links"></CardComponent>
+</template>
+<script setup lang="ts">
+import CardComponent from "@/components/media/CardComponent.vue";
+</script>
